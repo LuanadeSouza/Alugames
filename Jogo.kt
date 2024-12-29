c2908960c4ccccc25d6dd5596f08d0da68bd7d41
Luana <luana@wezo.com.br>
2024-12-29 14:35:15 -0300

    Projeto de Estudo Linguagem Kotlin

---
+++ Jogo.kt
@@ -0,0 +1,11 @@
+data class Jogo(val titulo:String,
+                val capa:String) {
+    var descricao: String? = null
+    override fun toString(): String {
+        return "Meu Jogo: \n" +
+                "Título: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao"
+
+    }
+
